f68ed2f4ed3e8db8a14955599b456210814c7dd6
PT <PT@PT-PC>
2018-07-12 16:40:34 +0200

    refactor

--- WEBPAGE/WebContent/scripts/slides.js
+++ WEBPAGE/WebContent/scripts/slides.js
@@ -2,7 +2,7 @@ var myIndex = 0;
 carousel();
 function carousel() {
 	var i;
-	var x = document.getElementsByClassName("mySlides");
+	var x = document.getElementsByClassName("slides");
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
@@ -11,5 +11,5 @@ function carousel() {
 		myIndex = 1
 	}
 	x[myIndex - 1].style.display = "block";
-	setTimeout(carousel, 2000); // Change image every 2 seconds
+	setTimeout(carousel, 2000);
 }
